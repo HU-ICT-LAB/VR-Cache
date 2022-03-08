0addb3acfe879eaaeca995cfef30cbd17de439ef
JaimieVos <jaimie.vos@outlook.com>
2022-03-08 12:21:01 +0100

    Setup voor het project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "aframe";
+import "aframe-react";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+	render() {
+		return (
+			<div className="App">
+				<a-scene>
+					<a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+					<a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+					<a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+					<a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+					<a-sky color="#ECECEC"></a-sky>
+				</a-scene>
+			</div>
+		);
+	}
 }
 
 export default App;
