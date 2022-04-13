806b6e7bdb62ed317251d055bd8ae2cd053df1ed
tvanoorschot <oorschot98@gmail.com>
2022-04-13 18:10:25 +0200

    Hide timer

--- src/components/menu/pause-component.js
+++ src/components/menu/pause-component.js
@@ -1,6 +1,7 @@
 window.AFRAME.registerComponent("pause-component", {
 	init() {
 		const timer = document.querySelector("#timer");
+		const display = document.querySelector("[displaytimer]");
 		const camera = this;
 
 		let pause = false;
@@ -8,13 +9,15 @@ window.AFRAME.registerComponent("pause-component", {
 		document.onkeydown = function menu(e) {
 			if (e.key === "Escape") {
 				if (!pause) {
+					display.setAttribute("visible","false");
+					pause = true;
 					timer.emit("paused");
 					camera.el.pause();
-					pause = true;
 				} else {
+					display.setAttribute("visible","true");
+					pause = false;
 					timer.emit("continued");
 					camera.el.play();
-					pause = false;
 				}
 			}
 		};
