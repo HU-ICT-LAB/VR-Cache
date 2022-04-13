f352d26de8ac62aacc0eff1040519d6ea4ecfb21
JaimieVos <jaimie.vos@outlook.com>
2022-04-13 20:18:56 +0200

    Refactoring

--- src/components/components.js
+++ src/components/components.js
@@ -1,4 +1,4 @@
 import "./cache-interaction";
-import "./Timer/timer-component";
-import "./Timer/display-timer";
+import "./timer/timer";
+import "./timer/display-timer";
 import "./menu/pause-component";
--- src/components/menu/pause-component.js
+++ src/components/menu/pause-component.js
@@ -1,7 +1,10 @@
+/**
+ * Component to pause and unpause the game
+ */
 window.AFRAME.registerComponent("pause-component", {
 	init() {
 		const timer = document.querySelector("#timer");
-		const display = document.querySelector("[displaytimer]");
+		const display = document.querySelector("[display-timer]");
 		const camera = this;
 
 		let pause = false;
@@ -9,19 +12,17 @@ window.AFRAME.registerComponent("pause-component", {
 		document.onkeydown = function menu(e) {
 			if (e.key === "Escape") {
 				if (!pause) {
-					display.setAttribute("visible","false");
+					display.setAttribute("visible", "false");
 					pause = true;
 					timer.emit("paused");
 					camera.el.pause();
 				} else {
-					display.setAttribute("visible","true");
+					display.setAttribute("visible", "true");
 					pause = false;
 					timer.emit("continued");
 					camera.el.play();
 				}
 			}
 		};
-
 	}
 });
-
