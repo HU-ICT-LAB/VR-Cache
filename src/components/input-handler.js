3396801f3f95e64fe5d6c7e8a9c2d44f26884598
tvanoorschot <oorschot98@gmail.com>
2022-05-08 22:33:31 +0200

    test

--- src/components/input-handler.js
+++ src/components/input-handler.js
@@ -1,9 +1,29 @@
+import {Vector3D} from "../utils/vector3d";
+
 window.AFRAME.registerComponent("input-handler", {
 	init() {
-		this.el.addEventListener("abuttondown", this.test);
+		this.el.addEventListener("abuttondown", this.interact);
 	},
 
-	test: function () {
-		document.getElementById("cache").object3D.position.x = 10;
+	interact() {
+		if (this.checkDistance()) {
+			document.getElementById("cache").object3D.position.x = 10;
+		}
+	},
+
+	/**
+	 * Checks if the player is in distance of the cache
+	 */
+	checkDistance() {
+		const playerPosition = document.querySelector("[camera]").object3D.position;
+		const cachePosition = document.getElementById("cache").object3D.position;
+
+		const result = new Vector3D(
+			cachePosition.x - playerPosition.x,
+			cachePosition.y - playerPosition.y,
+			cachePosition.z - playerPosition.z
+		);
+
+		return result.magnitude() <= 5;
 	}
 });
