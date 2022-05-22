c4eb2ffa1c4c6ab9fb57ac57052f5f66d012690d
RobertLanting <robertlanting02@gmail.com>
2022-05-22 18:41:40 +0200

    Update sonar-vibration.js

--- src/components/world/player/oculus/sonar-vibration.js
+++ src/components/world/player/oculus/sonar-vibration.js
@@ -1,27 +1,34 @@
 import {Vector3D} from "../../../../utils/vector3d";
 
-let pressed = true;
 window.AFRAME.registerComponent("vibration", {
+	pressed: false,
 	init() {
-		this.el.addEventListener("triggerdown", this.sonar.bind(this));
-		this.el.addEventListener("triggerup", this.sonarStop.bind(this));
+		this.el.addEventListener("triggerdown", () => {
+			this.pressed = true;
+		});
+		this.el.addEventListener("triggerup", () => {
+			this.pressed = false;
+		});
+		setInterval(this.sonar.bind(this), 1500);
 	},
 	sonar() {
-		const cachePosition = document.getElementById("rig").object3D.position;
-		const playerPosition = document.getElementById("cache").object3D.position;
+		let pulse;
+		if (this.pressed) {
+			const cachePosition = document.getElementById("rig").object3D.position;
+			const playerPosition = document.getElementById("cache").object3D.position;
 
-		const result = new Vector3D(
-			cachePosition.x - playerPosition.x,
-			cachePosition.y - playerPosition.y,
-			cachePosition.z - playerPosition.z
-		);
+			const result = new Vector3D(
+				cachePosition.x - playerPosition.x,
+				cachePosition.y - playerPosition.y,
+				cachePosition.z - playerPosition.z
+			);
 
-		this.el.components.haptics.pulse(0.5, result.magnitude() * 250);
-		if (pressed) {
-			setTimeout(this.sonar(), result.magnitude() * 251);
+			pulse = 5000 - result.magnitude() * 250;
+			if (pulse < 0) {
+				pulse = 0;
+			}
 		}
-	},
-	sonarStop() {
-		pressed = false;
+		this.el.components.haptics.pulse(0.5, pulse);
 	}
+
 });
