0111d6d3fb19171f6bd9a13e2ba40059a132893e
RobertLanting <robertlanting02@gmail.com>
2022-05-22 17:56:40 +0200

    vibration loop

--- src/components/world/player/oculus/sonar-vibration.js
+++ src/components/world/player/oculus/sonar-vibration.js
@@ -1,8 +1,10 @@
 import {Vector3D} from "../../../../utils/vector3d";
 
+let pressed = true;
 window.AFRAME.registerComponent("vibration", {
 	init() {
 		this.el.addEventListener("triggerdown", this.sonar.bind(this));
+		this.el.addEventListener("triggerup", this.sonarStop.bind(this));
 	},
 	sonar() {
 		const cachePosition = document.getElementById("rig").object3D.position;
@@ -13,6 +15,13 @@ window.AFRAME.registerComponent("vibration", {
 			cachePosition.y - playerPosition.y,
 			cachePosition.z - playerPosition.z
 		);
+
 		this.el.components.haptics.pulse(0.5, result.magnitude() * 250);
+		if (pressed) {
+			setTimeout(this.sonar(), result.magnitude() * 251);
+		}
+	},
+	sonarStop() {
+		pressed = false;
 	}
 });
