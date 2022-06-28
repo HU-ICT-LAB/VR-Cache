cf35685c44f437dadb41f6f72d4739f05d510c0d
JaimieVos <jaimie.vos@outlook.com>
2022-06-28 12:10:42 +0200

    Movement sound gefixt

--- src/components/world/player/player-sound.js
+++ src/components/world/player/player-sound.js
@@ -2,15 +2,15 @@ window.AFRAME.registerComponent("player-sound", {
 	walking: false,
 	alreadyWalking: false,
 	init() {
-		this.el.addEventListener("thumbstickmoved", this.isWalking);
+		this.el.addEventListener("thumbstickmoved", this.isWalking.bind(this));
 	},
 
 	isWalking(e) {
-		this.walking = e.detail.y > 0.95 || e.detail.y < -0.95 || e.detail.x < -0.95 || e.detail.x > 0.95;
-		console.log(e);
+		this.walking = e.detail.y != 0 || e.detail.x != 0;
 	},
 
 	tick() {
+		console.log(this.walking);
 		if (this.walking && !this.alreadyWalking) {
 			document.getElementById("playerWalkingsoundAsset").components.sound.playSound();
 			this.alreadyWalking = true;
